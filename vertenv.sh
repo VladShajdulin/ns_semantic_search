c940ed8470719774fdd4c06c8f38439a3cd79293
Vlad_Shajdulin <vladshajdulin@gmail.com>
2026-04-07 23:27:14 +0300

    изменения в структуре проекта

--- vertenv.sh
+++
@@ -1,5 +0,0 @@
-cd notebooks
-jupyter nbconvert --to *.ipynb
-pipreqs . --force --mode no-pin --savepath requirements.in --encoding=utf-8
-conda activate my-env
-pip-compile requirements.in --output-file=requirements.txt --no-annotate
